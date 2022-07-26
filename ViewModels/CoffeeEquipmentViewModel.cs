616e02849369ec2532ea1e53908a2fd12c84ccdf
Squidward Tenpoles <109726342+saintAldista@users.noreply.github.com>
2022-07-26 15:44:09 +0700

    Add files via upload
    Uploading file

---
+++ ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.ViewModels
+{
+    public class CoffeeEquipmentViewModel : BindableObject
+    {
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        public ICommand IncreaseCount { get; /*set;*/ }
+
+        int count = 0;
+        string countDisplay = "";
+
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay) return;
+                else
+                {
+                    countDisplay = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You've clicked me {count} times";
+        }
+    }
+}
