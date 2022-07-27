9f38f76fefe3355c0f4bd2d000b27b6535456cca
Squidward Tenpoles <109726342+saintAldista@users.noreply.github.com>
2022-07-27 10:04:36 +0700

    Update Models, ViewModels, View folder
    ListView

--- ViewModels/CoffeeEquipmentViewModel.cs
+++ ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,7 +1,10 @@
-using MvvmHelpers;
+﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
+using MyCoffeeApp.Models;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,37 +13,38 @@ namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : BasicViewModel
     {
-        public ObservableRangeCollection<string> Coffee { get; }
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+        public AsyncCommand RefreshCommand { get; }
         public CoffeeEquipmentViewModel()
         {
-            IncreaseCount = new Command(OnIncrease);
-            CallServerCommand = new AsyncCommand(CallServer);
-            Coffee = new ObservableRangeCollection<string>();
             Title = "Coffee Equipment";
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-picture-icon-png-image_695350.jpg";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", new[] { Coffee[2] }));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Take(2)));
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        public ICommand CallServerCommand { get; }
-        async Task CallServer()
+        async Task Refresh()
         {
-            var items = new List<string> { "Yes Plz", "Tonx", "Blue Bottle" };
-            Coffee.AddRange(items);
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            IsBusy = false;
         }
 
-        public ICommand IncreaseCount { get; /*set;*/ }
-
-        int count = 0;
-        string countDisplay = "";
-
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set => SetProperty(ref countDisplay, value);
-        }
-
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"You've clicked me {count} times";
-        }
     }
 }
--- Views/CoffeeEquipmentPage.xaml.cs
+++ Views/CoffeeEquipmentPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MyCoffeeApp.ViewModels;
+﻿using MyCoffeeApp.Models;
+using MyCoffeeApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,37 @@ namespace MyCoffeeApp.Views
             InitializeComponent();
             
             BindingContext = new CoffeeEquipmentViewModel();
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var coffee = ((ListView)sender).SelectedItem as Coffee;
+            if (coffee == null)
+            {
+                return;
+            }
+            else
+            {
+                await DisplayAlert("Coffee Selected", coffee.Name, "OK");
+            }
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var coffee = ((MenuItem)sender).BindingContext as Coffee;
+            if (coffee == null)
+            {
+                return;
+            }
+            else
+            {
+                await DisplayAlert("Coffee Selected", coffee.Name, "OK");
+            }
         }
     }
 }
