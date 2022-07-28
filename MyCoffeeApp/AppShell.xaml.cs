dc2b884f956888e5aa27960302ad9eda99b916ba
Squidward Tenpoles <109726342+saintAldista@users.noreply.github.com>
2022-07-28 10:05:22 +0700

    Add Login Flow
    Add some files and update some codes

--- MyCoffeeApp/AppShell.xaml.cs
+++ MyCoffeeApp/AppShell.xaml.cs
@@ -14,6 +14,8 @@ namespace MyCoffeeApp
 
             Routing.RegisterRoute(nameof(MyCoffeeDetailsPage),
                 typeof(MyCoffeeDetailsPage));
+            Routing.RegisterRoute(nameof(RegistrationPage),
+                typeof(RegistrationPage));
         }
     }
 }
--- MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -16,5 +16,9 @@ namespace MyCoffeeApp.Views
         {
             InitializeComponent();
         }
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
     }
 }
