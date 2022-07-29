6f61063157bcb1fda43e383afaf9f9c766402381
Squidward Tenpoles <109726342+saintAldista@users.noreply.github.com>
2022-07-29 14:22:38 +0700

    Adding Image Caching

---
+++ MyCoffeeApp/ViewModels/ImageCacheViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.ViewModels
+{
+    public class ImageCacheViewModel : ViewModelBase
+    {
+        public UriImageSource Image { get; set; } =
+            new UriImageSource
+            {
+                Uri = new Uri("https://images.wsdot.wa.gov/nw/090vc00579.jpg"),
+                CachingEnabled = true,
+                CacheValidity = TimeSpan.FromMinutes(1)
+            };
+
+        public Command RefreshCommand { get; }
+
+        public ImageCacheViewModel()
+        {
+            RefreshCommand = new Command(() =>
+            {
+                Image = new UriImageSource
+                {
+                    Uri = new Uri("https://images.wsdot.wa.gov/nw/090vc00579.jpg"),
+                    CachingEnabled = true,
+                    CacheValidity = TimeSpan.FromMinutes(1)
+                };
+                OnPropertyChanged(nameof(Image));
+            });
+        }
+
+    }
+}
